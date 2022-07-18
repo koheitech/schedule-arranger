fd06e0fca925a89f0f6c8f9934a69690c9fe8a39
Kohei Miyamoto <koheitechigh@gmail.com>
2022-07-19 05:20:40 +0900

    set sequelize for data modeling

---
+++ models/sequelize-loader.js
@@ -0,0 +1,8 @@
+'use strict';
+const {Sequelize, DataTypes} = require('sequelize');
+const sequelize = new Sequelize('postgres://postgres:postgres@db/schedule_arranger');
+
+module.exports = {
+  sequelize,
+  DataTypes
+};
