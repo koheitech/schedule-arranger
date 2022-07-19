9afecce953b664446ea57cfe0f15bf1a2a0360a7
Kohei Miyamoto <koheitechigh@gmail.com>
2022-07-19 15:27:02 +0900

    create Web API for updating availability

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ var indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
 const logoutRouter = require('./routes/logout');
 const scheduleRouter = require('./routes/schedules');
+const availabilityRouter = require('./routes/availabilities');
 
 var app = express();
 app.use(helmet());
@@ -82,6 +83,7 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 app.use('/schedules', scheduleRouter);
+app.use('/schedules', availabilityRouter);
 
 app.get('/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const passportStub = require('passport-stub');
 const User = require('../models/user');
 const Schedule = require('../models/schedule');
 const Candidate = require('../models/candidate');
-const { RowDescriptionMessage } = require('pg-protocol/dist/messages');
+const Availability = require('../models/availability');
 
 describe('/login', () => {
   beforeAll(() => {
@@ -54,16 +54,7 @@ describe('/schedules', () => {
     passportStub.logout();
     passportStub.uninstall();
 
-    // Delete test date
-    const candidates = await Candidate.findAll({
-      where: { scheduleId: scheduleId }
-    });
-    const promises = candidates.map(c => {
-      return c.destroy();
-    });
-    await Promise.all(promises);
-    const s = await Schedule.findByPk(scheduleId);
-    await s.destroy();
+    await deleteScheduleAggregate(scheduleId);
   });
 
   test('create and display schedule', async () => {
@@ -90,3 +81,67 @@ describe('/schedules', () => {
       .expect(200);
   });
 });
+
+describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
+  let scheduleId = '';
+  beforeAll(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  afterAll(async () => {
+    passportStub.logout();
+    passportStub.uninstall();
+    await deleteScheduleAggregate(scheduleId);
+  });
+
+  test('update availabilities', async() => {
+    await User.upsert({ userId: 0, username: 'testuser'});
+    const res = await request(app)
+      .post('/schedules')
+      .send({ scheduleName: 'test schedule 1', candidates: 'can1'});
+    const createdSchedulePath = res.headers.location;
+    scheduleId = createdSchedulePath.split('/schedules/')[1];
+    const candidate = await Candidate.findOne({
+      where: { scheduleId: scheduleId }
+    });
+
+    const userId = 0;
+    await request(app)
+      .post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidate.candidateId}`)
+      .send({ availability: 2}) // 2 => present
+      .expect('{"status":"OK","availability":2}');
+
+    // test if the updated availabilities are reflected onto database
+    const { availability } = await Availability.findOne({
+      where: { scheduleId: scheduleId }
+    });
+    expect(availability).toBe(2);
+  });
+});
+
+async function deleteScheduleAggregate(scheduleId) {
+  // Delete test availabilities
+  const availabilities = await Availability.findAll({
+    where: { scheduleId: scheduleId }
+  });
+
+  const promiseAvailabilityDestroy = availabilities.map(availability => {
+    return availability.destroy();
+  });
+
+  await Promise.all(promiseAvailabilityDestroy);
+  
+  // Delete test candidates
+  const candidates = await Candidate.findAll({
+    where: { scheduleId: scheduleId }
+  });
+  const promiseCandidateDestroy = candidates.map(candidate => {
+    return candidate.destroy();
+  });
+
+  await Promise.all(promiseCandidateDestroy);
+
+  const schedule = await Schedule.findByPk(scheduleId);
+  await schedule.destroy();
+}
