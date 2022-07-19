6a5e0695ea5657bc7aaf38318321689b335f0789
Kohei Miyamoto <koheitechigh@gmail.com>
2022-07-19 14:42:11 +0900

    add test for creating schedule

--- test/test.js
+++ test/test.js
@@ -2,6 +2,10 @@
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+const User = require('../models/user');
+const Schedule = require('../models/schedule');
+const Candidate = require('../models/candidate');
+const { RowDescriptionMessage } = require('pg-protocol/dist/messages');
 
 describe('/login', () => {
   beforeAll(() => {
@@ -36,5 +40,53 @@ describe('/logout', () => {
       .get('/logout')
       .expect('Location', '/')
       .expect(302);
+  });
+});
+
+describe('/schedules', () => {
+  let scheduleId = '';
+  beforeAll(() => {
+    passportStub.install(app);
+    passportStub.login({ id: 0, username: 'testuser' });
+  });
+
+  afterAll(async () => {
+    passportStub.logout();
+    passportStub.uninstall();
+
+    // Delete test date
+    const candidates = await Candidate.findAll({
+      where: { scheduleId: scheduleId }
+    });
+    const promises = candidates.map(c => {
+      return c.destroy();
+    });
+    await Promise.all(promises);
+    const s = await Schedule.findByPk(scheduleId);
+    await s.destroy();
+  });
+
+  test('create and display schedule', async () => {
+    await User.upsert({ userId: 0, username: 'testuser' });
+    const res = await request(app)
+      .post('/schedules')
+      .send({
+        scheduleName: 'test schedule 1',
+        memo: 'test memo1\r\ntest memo2',
+        candidates: 'test can1\r\ntest can2'
+      })
+      .expect('Location', /schedules/)
+      .expect(302);
+
+    const createdSchedulePath = res.headers.location;
+    scheduleId = createdSchedulePath.split('/schedules/')[1];
+    await request(app)
+      .get(createdSchedulePath)
+      .expect(/test schedule 1/)
+      .expect(/test memo1/)
+      .expect(/test memo2/)
+      .expect(/test can1/)
+      .expect(/test can2/)
+      .expect(200);
   });
 });
