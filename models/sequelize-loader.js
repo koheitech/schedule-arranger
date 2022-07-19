ad52a3e97d5c66f7b351479cd5b69ff0bad7695c
Kohei Miyamoto <koheitechigh@gmail.com>
2022-07-20 05:38:37 +0900

    prepare for HEROKU

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,6 +1,30 @@
 'use strict';
 const {Sequelize, DataTypes} = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:postgres@db/schedule_arranger');
+
+const dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+};
+
+const sequelize = process.env.DATABASE_URL ?
+  // Production env
+  new Sequelize(
+    proecss.env.DATABASE_URL,
+    {
+      logging: false,
+      dialectOptions
+    }
+  )
+  :
+  // Development env
+  new Sequelize(
+    'postgres://postgres:postgres@db/schedule_arranger',
+    {
+      logging: false
+    }
+  );
 
 module.exports = {
   sequelize,
